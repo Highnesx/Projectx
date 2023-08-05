70ef7f30673bbb099dc26306ecdd738268a3a657
Highnesx <gosifehighness@gmail.com>
2023-08-04 17:33:10 -0700

    Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - function that prints the number of arguments passed into it
+* @argc: parameter one
+* @argv: parameter 2
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+
+}
