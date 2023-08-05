9d9871f7a90329ead126696f523235693f47dc42
Highnesx <gosifehighness@gmail.com>
2023-08-04 17:08:53 -0700

    Argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - fuction that renames a program
+* @argc: first parameter
+* @argv: second parameter
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
