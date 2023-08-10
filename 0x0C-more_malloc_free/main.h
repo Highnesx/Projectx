fbfb5aff0e770a61a356e3d5639e935b3a979ec4
Highnesx <gosifehighness@gmail.com>
2023-08-10 22:53:38 +0100

    calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
 
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -7,5 +8,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
